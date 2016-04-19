0ca302886efcf5cda26f05d679a7734d7cb64fb6
Carlos Galdamez <cgaldamez14@gmail.com>
2016-04-19 13:24:03 -0700

    initial commit

---
+++ src/chatroom/ClientHandler.java
@@ -0,0 +1,74 @@
+package chatroom;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClientHandler {
+
+	private Socket socket;
+	private PrintWriter output;
+	private BufferedReader input;
+
+
+	public ClientHandler(String ip, int port) throws IOException{
+		this.socket = new Socket(ip, port);
+		input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+		output = new PrintWriter(this.socket.getOutputStream(), true);
+		
+		Thread read = new Thread(){
+			public void run() {
+				while(true){
+					try {
+						if(input.ready())
+							System.out.println(input.readLine());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				}
+			}
+		};
+		
+		read.start();
+	} 
+
+	public ClientHandler(Socket socket, String name) throws IOException{
+		this.socket = socket;
+		input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+		output = new PrintWriter(this.socket.getOutputStream(), true);
+
+		Thread read = new Thread(){
+			public void run() {
+				while(true){
+					try {
+						if(input.ready())
+							System.out.println(input.readLine());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				}
+			}
+		};
+		
+		read.start();
+	} 
+
+
+	// Will be used to send messages to other rovers
+	public void send(String message){
+		output.println(message);
+	}
+
+	public InetAddress getIP(){
+		return socket.getInetAddress();
+	}
+
+	public int getPort(){
+		return socket.getPort();
+	}
+}
---
+++ src/chatroom/Server.java
@@ -0,0 +1,103 @@
+package chatroom;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server implements Runnable{
+	// Rover name and listerning port
+		private final static String NAME = "ROVER_03";
+		private final static int PORT = 8000;
+
+		// List of connected rovers
+		private List<ClientHandler> clients;
+
+
+		private ServerSocket serverSocket;
+
+		// Will be used to get input from console
+		private InputStreamReader cin;
+		private StringBuilder message = new StringBuilder();
+
+		public Server() throws IOException{
+			// Creates a server socket at specified port
+			serverSocket = new ServerSocket(PORT);
+
+			System.out.println("ROVER_03 server online...");
+			System.out.println("Waiting for other rovers to connect...");
+
+			cin = new InputStreamReader(System.in);
+			clients= new ArrayList<>();
+
+			// Begin messaging thread
+			sendMessage();
+		}
+
+		@Override
+		public void run() {
+			while(true){
+				try {
+					ClientHandler client = new ClientHandler(serverSocket.accept(), NAME);
+					clients.add(client);
+					getClientList();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		public void sendMessage(){
+			Thread messages = new Thread(){
+				public void run(){
+					while(true){
+						//					try {
+						//						if(cin.ready()){
+						//							System.out.println((char)cin.read());
+						//						}
+						//					} catch (IOException e1) {
+						//						// TODO Auto-generated catch block
+						//						e1.printStackTrace();
+						//					}
+
+						try {
+							//						System.out.println(cin.ready());
+							//						Thread.sleep(5000);
+							if(cin.ready())
+								if(!clients.isEmpty()){
+
+									while(cin.ready()){
+										message.append((char)cin.read());
+										System.out.println(cin.ready());
+									}
+
+									for(ClientHandler c : clients){
+										System.out.println(message.toString());
+										System.out.println("sending to " + c.getPort() + " message: " + message );
+										c.send(message.toString());
+									}
+									message.setLength(0); // Reset StringBuilder
+								}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			};
+			messages.start();
+		}
+
+		public void getClientList(){
+			System.out.println("****************** Rover List **********************");
+			for(ClientHandler c : clients){
+				System.out.println("Address: " + c.getIP() + " , Port:" + c.getPort());
+			}
+		}
+
+		public static void main(String args[]) throws IOException{
+			Thread newThread = new Thread(new Server());
+			newThread.start();
+		}
+}
