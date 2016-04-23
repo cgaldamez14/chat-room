e32ad2552c2066ca881a496b2f8b72c939e8fcd4
Carlos Galdamez <cgaldamez14@gmail.com>
2016-04-23 11:17:37 -0700

    forgot to make the Disconnect class Serializable

--- src/chatroom/Disconnect.java
+++ src/chatroom/Disconnect.java
@@ -1,7 +1,13 @@
 package chatroom;
 
-public class Disconnect {
+import java.io.Serializable;
+
+public class Disconnect implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String message;
 	
 	public Disconnect(String message){
